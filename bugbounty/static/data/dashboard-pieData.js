202bd4b571f3aa642861d3b200125cb3ed75e9c0
Admin <vincent.belotti@etu.imt-lille-douai.fr>
2019-09-26 11:16:57 +0200

    Up

--- bugbounty/static/data/dashboard-pieData.js
+++
@@ -1,8 +0,0 @@
-var pieData = {
-	datasets: [{
-		data: [7, 21, 18],
-		backgroundColor : [red, orange, yellow]
-	}],
-// These labels appear in the legend and in the tooltips when hovering different arcs
-labels: ['Majeures', 'Modérées', 'Mineures']
-};
--- bugbounty/static/data/perimetre-1-pieData.js
+++
@@ -1,7 +0,0 @@
-var pieData = {
-	datasets: [{
-		data: [1, 3, 1],
-		backgroundColor : [red, orange, yellow]
-	}],
-	labels: ['Majeures', 'Modérées', 'Mineures']
-};
